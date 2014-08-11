770699882f3979e89da190594dc019860611e8d6
hgcummings <harry@xlr3.com>
2014-08-11 19:52:31 +0100

    Attempt to cache dependencies between builds

---
+++ build/cache_dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+tar -cjf cached-dependencies.tar.bz2 $HOME/.m2
+aws s3 cp cached-dependencies.tar.bz2 s3://exwhy-dependencies/cached.tar.bz2
---
+++ build/get_cached_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd $HOME/.m2
+aws s3 cp s3://exwhy-dependencies/cached.tar.bz2 cached-dependencies.tar.bz2
+tar -xjf cached-dependencies.tar.bz2
