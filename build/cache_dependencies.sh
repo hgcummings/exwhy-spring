d83c00761037a9963aa35adcc807d80314da2ff3
hgcummings <harry@xlr3.com>
2014-08-11 22:05:39 +0100

    Add bash shebang line to scripts. Make executable again.

--- build/cache_dependencies.sh
+++ build/cache_dependencies.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 tar -cjf cached-dependencies.tar.bz2 $HOME/.m2
 aws s3 cp cached-dependencies.tar.bz2 s3://exwhy-dependencies/cached.tar.bz2
--- build/get_cached_dependencies.sh
+++ build/get_cached_dependencies.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd $HOME/.m2
 aws s3 cp s3://exwhy-dependencies/cached.tar.bz2 cached-dependencies.tar.bz2
 tar -xjf cached-dependencies.tar.bz2
