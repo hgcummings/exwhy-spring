723bffc8da59759614bd373e89b201decf2abd61
hgcummings <harry@xlr3.com>
2014-08-12 01:28:45 +0100

    Correct bucket name in .travis.yml

--- build/get_cached_dependencies.sh
+++ build/get_cached_dependencies.sh
@@ -23,5 +23,4 @@ curl \
   -H "X-amz-date: ${timeStamp}"\
   -H "Authorization: AWS4-HMAC-SHA256 Credential=$AWS_ACCESS_KEY_ID/${scope},SignedHeaders=host;x-amz-content-sha256;x-amz-date,Signature=${signature}" \
   http://${bucket}.s3.amazonaws.com${file} > cached.tar.bz2
-cat cached.tar.bz2
 tar -xjf cached.tar.bz2
