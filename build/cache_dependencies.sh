24ee64a69b75998a1cc5e65365baf9ebeed1e7c0
hgcummings <harry@xlr3.com>
2014-08-12 01:23:59 +0100

    Cache dependencies in S3

--- build/cache_dependencies.sh
+++ build/cache_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tar -cjf cached-dependencies.tar.bz2 $HOME/.m2
-aws s3 cp cached-dependencies.tar.bz2 s3://exwhy-dependencies/cached.tar.bz2
+md dependencies
+tar -cjf dependencies/cached.tar.bz2 $HOME/.m2
--- build/get_cached_dependencies.sh
+++ build/get_cached_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bucket="exwhy-dependencies"
+bucket=$AWS_BUCKET
 file="/cached.tar.bz2"
 region="us-east-1"
 service="s3"
@@ -10,17 +10,17 @@ scope=${dateStamp}/${region}/${service}/aws4_request
 canonicalRequest="GET\n${file}\n\nhost:${bucket}.s3.amazonaws.com\nx-amz-content-sha256:${emptyHash}\nx-amz-date:${timeStamp}\n\nhost;x-amz-content-sha256;x-amz-date\n${emptyHash}"
 hashedRequest=`echo -en ${canonicalRequest} | openssl sha256 | cut -d ' ' -f 2`
 stringToSign="AWS4-HMAC-SHA256\n${timeStamp}\n${scope}\n${hashedRequest}"
-dateKey=`echo -en ${dateStamp} | openssl sha256 -hmac AWS4${AWS_SECRET_ACCESS_KEY} | cut -d ' ' -f 2`
-dateRegionKey=`echo -en ${region} | openssl sha256 -hmac ${dateKey} | cut -d ' ' -f 2`
-dateRegionServiceKey=`echo -en ${service} | openssl sha256 -hmac ${dateRegionKey} | cut -d ' ' -f 2`
-signingKey=`echo -en aws4_request | openssl sha256 -hmac ${dateRegionServiceKey} | cut -d ' ' -f 2`
-signature=`echo -en ${stringToSign} | openssl sha256 -hmac ${signingKey} | cut -d ' ' -f 2`
+initialKey=`echo -n "AWS4${AWS_SECRET_ACCESS_KEY}" | xxd -c 256 -ps`
+dateKey=`echo -n ${dateStamp} | openssl dgst -sha256 -mac Hmac -macopt hexkey:${initialKey} | cut -d ' ' -f 2`
+dateRegionKey=`echo -n ${region} | openssl dgst -sha256 -mac Hmac -macopt hexkey:${dateKey} | cut -d ' ' -f 2`
+dateRegionServiceKey=`echo -n ${service} | openssl dgst -sha256 -mac Hmac -macopt hexkey:${dateRegionKey} | cut -d ' ' -f 2`
+signingKey=`echo -n "aws4_request" | openssl dgst -sha256 -mac Hmac -macopt hexkey:${dateRegionServiceKey} | cut -d ' ' -f 2`
+signature=`echo -en ${stringToSign} | openssl dgst -sha256 -mac Hmac -macopt hexkey:${signingKey} | cut -d ' ' -f 2`
 cd $HOME/.m2
 curl \
   -H "Host: ${bucket}.s3.amazonaws.com" \
   -H "X-amz-content-sha256: ${emptyHash}" \
   -H "X-amz-date: ${timeStamp}"\
   -H "Authorization: AWS4-HMAC-SHA256 Credential=$AWS_ACCESS_KEY_ID/${scope},SignedHeaders=host;x-amz-content-sha256;x-amz-date,Signature=${signature}" \
-  http://${bucket}.s3.amazonaws.com${file}
+  http://${bucket}.s3.amazonaws.com${file} > cached.tar.bz2
 tar -xjf cached.tar.bz2
-ls
