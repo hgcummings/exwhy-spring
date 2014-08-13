dcf352b41d2df59f3d204cc5e717143fb609d234
hgcummings <harry@xlr3.com>
2014-08-14 00:16:21 +0100

    Minor cleanup of S3 caching script

--- build/s3_caching.sh
+++ build/s3_caching.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 dependencyFolder=$HOME/.m2
 file="/cached.tar.bz2"
-region="us-east-1"
 tempFolder="dependencies"
 
-service="s3"
 timeStamp=$(date -u +%Y%m%dT%H%M%SZ)
 dateStamp=$(date -u +%Y%m%d)
+region="us-east-1"
+service="s3"
 scope=${dateStamp}/${region}/${service}/aws4_request
 host="${AWS_BUCKET}.s3.amazonaws.com"
+credential="${AWS_ACCESS_KEY_ID}/${scope}"
+key=$(echo -n "AWS4${AWS_SECRET_ACCESS_KEY}" | xxd -c 256 -ps)
+signedHeaders="host;x-amz-content-sha256;x-amz-date"
 url="http://${host}${file}"
 
 function cacheDependencies {
@@ -28,7 +31,7 @@ function compressDependencies {
 
 function uploadArchive {
     contentHash=$(cat dependencies${file} | openssl sha256 | trimOpenSslOutput)
-    authHeader=$(generateAuthHeaderForMethod "PUT")
+    authHeader=$(generateAuthHeaderForHttpMethod "PUT")
     curl -X PUT -T "dependencies${file}" \
       -H "Host: ${host}" \
       -H "X-amz-content-sha256: ${contentHash}" \
@@ -39,7 +42,7 @@ function uploadArchive {
 
 function downloadArchive {
     contentHash="e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-    authHeader=$(generateAuthHeaderForMethod "GET")
+    authHeader=$(generateAuthHeaderForHttpMethod "GET")
     responseCode=$(curl \
       -H "Host: ${host}" \
       -H "X-amz-content-sha256: ${contentHash}" \
@@ -59,18 +62,19 @@ function extractDependencies {
     fi
 }
 
-function generateAuthHeaderForMethod {
-    echo $(generateAuthHeaderForCanonicalRequest \
-      "$1\n${file}\n\nhost:${host}\nx-amz-content-sha256:${contentHash}\nx-amz-date:${timeStamp}\n\nhost;x-amz-content-sha256;x-amz-date\n${contentHash}")
+function generateAuthHeaderForHttpMethod {
+    canonicalRequest="$1\n${file}\n\n"
+    canonicalRequest+="host:${host}\nx-amz-content-sha256:${contentHash}\nx-amz-date:${timeStamp}\n\n"
+    canonicalRequest+="${signedHeaders}\n${contentHash}"
+    echo $(generateAuthHeaderForCanonicalRequest ${canonicalRequest})
 }
 
 function generateAuthHeaderForCanonicalRequest {
     hashedRequest=$(echo -en $1 | openssl sha256 | trimOpenSslOutput)
     stringToSign="AWS4-HMAC-SHA256\n${timeStamp}\n${scope}\n${hashedRequest}"
-    key=$(echo -n "AWS4${AWS_SECRET_ACCESS_KEY}" | xxd -c 256 -ps)
     signature=$(sha256Hash $(sha256Hash $(sha256Hash $(sha256Hash $(sha256Hash \
       ${key} ${dateStamp}) ${region}) ${service}) "aws4_request") ${stringToSign})
-    echo "AWS4-HMAC-SHA256 Credential=$AWS_ACCESS_KEY_ID/${scope},SignedHeaders=host;x-amz-content-sha256;x-amz-date,Signature=${signature}"
+    echo "AWS4-HMAC-SHA256 Credential=${credential},SignedHeaders=${signedHeaders},Signature=${signature}"
 }
 
 function sha256Hash {
