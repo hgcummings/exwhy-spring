0157bcec10ff1e374501a2bc6d65a030d4070efd
hgcummings <harry@xlr3.com>
2014-08-15 00:01:07 +0100

    Add debug output to deploy script

--- build/heroku_deploy.sh
+++ build/heroku_deploy.sh
@@ -14,11 +14,15 @@ response=$(curl -X POST \
 -d '{"process_types":{"web": "java $JAVA_OPTS -cp ./classes:./lib/* io.hgc.exwhy.web.Application"}}' \
 -n https://api.heroku.com/apps/${APP_NAME}/slugs)
 
+echo ${response}
+
 function parseField {
     echo -ne $2 | grep -o "\"$1\"\s*:\s*\"[^\"]*\"" | head -1 | cut -d '"' -f 4
 }
 s3Url=$(parseField "url" "'${response}'")
+echo ${s3Url}
 slugId=$(parseField "id" "'${response}'")
+echo ${slugId}
 
 # Upload archive
 curl -X PUT -H "Content-Type:" --data-binary @slug.tgz ${s3Url}
