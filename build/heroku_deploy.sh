bd8a6c03474b7a954d9b3beb017a02ed0114f858
hgcummings <harry@xlr3.com>
2014-08-23 14:47:10 +0100

    Namespace variables and functions in source-included scripts

--- build/heroku_deploy.sh
+++ build/heroku_deploy.sh
@@ -8,33 +8,33 @@ mkdir "app/.profile.d" && echo 'export PATH="/app/.jre/bin:$PATH"' >> app/.profi
 
 # Archive slug
 tar czfv slug.tgz ./app
-apiKey=`echo ":${HEROKU_API_KEY}" | base64`
+_heroku_deploy_apiKey=`echo ":${HEROKU_API_KEY}" | base64`
 
 # Create slug object
-response=$(curl -X POST \
+_heroku_deploy_createSlugResponse=$(curl -X POST \
 -H "Content-Type: application/json" \
 -H "Accept: application/vnd.heroku+json; version=3" \
--H "Authorization: ${apiKey}" \
+-H "Authorization: ${_heroku_deploy_apiKey}" \
 -d '{"process_types":{"web": "java $JAVA_OPTS -cp ./classes:./lib/* io.hgc.exwhy.web.Application"}}' \
 -n https://api.heroku.com/apps/${HEROKU_APP_NAME_STAGE}/slugs)
 
-function parseField {
+function _heroku_deploy_parseField {
     echo -ne $2 | grep -o "\"$1\"\s*:\s*\"[^\"]*\"" | head -1 | cut -d '"' -f 4
 }
-s3Url=$(parseField "url" "'${response}'")
-slugId=$(parseField "id" "'${response}'")
+_heroku_deploy_slugS3Url=$(_heroku_deploy_parseField "url" "'${_heroku_deploy_createSlugResponse}'")
+_heroku_deploy_slugId=$(_heroku_deploy_parseField "id" "'${_heroku_deploy_createSlugResponse}'")
 
 # Upload archive
-curl -X PUT -H "Content-Type:" --data-binary @slug.tgz ${s3Url}
+curl -X PUT -H "Content-Type:" --data-binary @slug.tgz ${_heroku_deploy_slugS3Url}
 
 # Deploy slug
-function deploy {
+function deployToHeroku {
     curl -X POST \
     -H "Content-Type: application/json" \
     -H "Accept: application/vnd.heroku+json; version=3" \
-    -H "Authorization: ${apiKey}" \
-    -d "{\"slug\":\"${slugId}\"}" \
+    -H "Authorization: ${_heroku_deploy_apiKey}" \
+    -d "{\"slug\":\"${_heroku_deploy_slugId}\"}" \
     -n https://api.heroku.com/apps/$1/releases
 }
 
-deploy ${HEROKU_APP_NAME_STAGE}
+deployToHeroku ${HEROKU_APP_NAME_STAGE}
--- build/s3_caching.sh
+++ build/s3_caching.sh
@@ -1,96 +1,96 @@
 #!/bin/bash
-dependencyFolder=$HOME/.m2
-file="/cached.tar.bz2"
-tempFolder="dependencies"
+_s3_caching_dependencyFolder=$HOME/.m2
+_s3_caching_file="/cached.tar.bz2"
+_s3_caching_tempFolder="dependencies"
 
-timeStamp=$(date -u +%Y%m%dT%H%M%SZ)
-dateStamp=$(date -u +%Y%m%d)
-region="us-east-1"
-service="s3"
-scope=${dateStamp}/${region}/${service}/aws4_request
-host="${AWS_BUCKET}.s3.amazonaws.com"
-credential="${AWS_ACCESS_KEY_ID}/${scope}"
-key=$(echo -n "AWS4${AWS_SECRET_ACCESS_KEY}" | xxd -c 256 -ps)
-signedHeaders="host;x-amz-content-sha256;x-amz-date"
-url="http://${host}${file}"
+_s3_caching_timeStamp=$(date -u +%Y%m%dT%H%M%SZ)
+_s3_caching_dateStamp=$(date -u +%Y%m%d)
+_s3_caching_region="us-east-1"
+_s3_caching_service="s3"
+_s3_caching_scope=${_s3_caching_dateStamp}/${_s3_caching_region}/${_s3_caching_service}/aws4_request
+_s3_caching_host="${AWS_BUCKET}.s3.amazonaws.com"
+_s3_caching_credential="${AWS_ACCESS_KEY_ID}/${_s3_caching_scope}"
+_s3_caching_key=$(echo -n "AWS4${AWS_SECRET_ACCESS_KEY}" | xxd -c 256 -ps)
+_s3_caching_signedHeaders="host;x-amz-content-sha256;x-amz-date"
+_s3_caching_url="http://${_s3_caching_host}${_s3_caching_file}"
 
 function getCachedDependencies {
-    if [[ -z $(diffPomFiles) ]]; then
+    if [[ -z $(_s3_caching_diffPomFiles) ]]; then
         echo "pom.xml files unchanged - using cached dependencies"
-        downloadArchive
-        extractDependencies
+        _s3_caching_downloadArchive
+        _s3_caching_extractDependencies
     fi
 }
 
 function cacheDependencies {
-    if [[ -n $(diffPomFiles) ]]; then
+    if [[ -n $(_s3_caching_diffPomFiles) ]]; then
         echo "pom.xml files have changed - updating cached dependencies"
-        compressDependencies
-        uploadArchive
+        _s3_caching_compressDependencies
+        _s3_caching_uploadArchive
     fi
 }
 
-function diffPomFiles {
+function _s3_caching_diffPomFiles {
     git diff ${TRAVIS_COMMIT_RANGE} pom.xml **/pom.xml
 }
 
-function downloadArchive {
-    contentHash="e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-    authHeader=$(generateAuthHeaderForHttpMethod "GET")
-    responseCode=$(curl \
-      -H "Host: ${host}" \
-      -H "X-amz-content-sha256: ${contentHash}" \
-      -H "X-amz-date: ${timeStamp}" \
-      -H "Authorization: ${authHeader}" \
-      -o .${file} \
+function _s3_caching_downloadArchive {
+    _s3_caching_contentHash="e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    _s3_caching_authHeader=$(_s3_caching_generateAuthHeaderForHttpMethod "GET")
+    _s3_caching_responseCode=$(curl \
+      -H "Host: ${_s3_caching_host}" \
+      -H "X-amz-content-sha256: ${_s3_caching_contentHash}" \
+      -H "X-amz-date: ${_s3_caching_timeStamp}" \
+      -H "Authorization: ${_s3_caching_authHeader}" \
+      -o .${_s3_caching_file} \
       -w "%{http_code}" \
-      ${url})
+      ${_s3_caching_url})
 }
 
-function extractDependencies {
-    if test ${responseCode} -ne 200; then
-        echo -n "${responseCode} "
-        cat .${file}
+function _s3_caching_extractDependencies {
+    if test ${_s3_caching_responseCode} -ne 200; then
+        echo -n "${_s3_caching_responseCode} "
+        cat .${_s3_caching_file}
     else
-        tar --no-overwrite-dir -xjf .${file} -C /
+        tar --no-overwrite-dir -xjf .${_s3_caching_file} -C /
     fi
 }
 
-function compressDependencies {
-    mkdir ${tempFolder}
-    tar -cjf ${tempFolder}${file} ${dependencyFolder}
+function _s3_caching_compressDependencies {
+    mkdir ${_s3_caching_tempFolder}
+    tar -cjf ${_s3_caching_tempFolder}${_s3_caching_file} ${_s3_caching_dependencyFolder}
 }
 
-function uploadArchive {
-    contentHash=$(cat dependencies${file} | openssl sha256 | trimOpenSslOutput)
-    authHeader=$(generateAuthHeaderForHttpMethod "PUT")
-    curl -X PUT -T "dependencies${file}" \
-      -H "Host: ${host}" \
-      -H "X-amz-content-sha256: ${contentHash}" \
-      -H "X-amz-date: ${timeStamp}" \
-      -H "Authorization: ${authHeader}" \
-      ${url}
+function _s3_caching_uploadArchive {
+    _s3_caching_contentHash=$(cat dependencies${_s3_caching_file} | openssl sha256 | _s3_caching_trimOpenSslOutput)
+    _s3_caching_authHeader=$(_s3_caching_generateAuthHeaderForHttpMethod "PUT")
+    curl -X PUT -T "dependencies${_s3_caching_file}" \
+      -H "Host: ${_s3_caching_host}" \
+      -H "X-amz-content-sha256: ${_s3_caching_contentHash}" \
+      -H "X-amz-date: ${_s3_caching_timeStamp}" \
+      -H "Authorization: ${_s3_caching_authHeader}" \
+      ${_s3_caching_url}
 }
 
-function generateAuthHeaderForHttpMethod {
-    canonicalRequest="$1\n${file}\n\n"
-    canonicalRequest+="host:${host}\nx-amz-content-sha256:${contentHash}\nx-amz-date:${timeStamp}\n\n"
-    canonicalRequest+="${signedHeaders}\n${contentHash}"
-    echo $(generateAuthHeaderForCanonicalRequest ${canonicalRequest})
+function _s3_caching_generateAuthHeaderForHttpMethod {
+    _s3_caching_canonicalRequest="$1\n${_s3_caching_file}\n\n"
+    _s3_caching_canonicalRequest+="host:${_s3_caching_host}\nx-amz-content-sha256:${_s3_caching_contentHash}\nx-amz-date:${_s3_caching_timeStamp}\n\n"
+    _s3_caching_canonicalRequest+="${_s3_caching_signedHeaders}\n${_s3_caching_contentHash}"
+    echo $(_s3_caching_generateAuthHeaderForCanonicalRequest ${_s3_caching_canonicalRequest})
 }
 
-function generateAuthHeaderForCanonicalRequest {
-    hashedRequest=$(echo -en $1 | openssl sha256 | trimOpenSslOutput)
-    stringToSign="AWS4-HMAC-SHA256\n${timeStamp}\n${scope}\n${hashedRequest}"
-    signature=$(sha256Hash $(sha256Hash $(sha256Hash $(sha256Hash $(sha256Hash \
-      ${key} ${dateStamp}) ${region}) ${service}) "aws4_request") ${stringToSign})
-    echo "AWS4-HMAC-SHA256 Credential=${credential},SignedHeaders=${signedHeaders},Signature=${signature}"
+function _s3_caching_generateAuthHeaderForCanonicalRequest {
+    _s3_caching_hashedRequest=$(echo -en $1 | openssl sha256 | _s3_caching_trimOpenSslOutput)
+    _s3_caching_stringToSign="AWS4-HMAC-SHA256\n${_s3_caching_timeStamp}\n${_s3_caching_scope}\n${_s3_caching_hashedRequest}"
+    _s3_caching_signature=$(_s3_caching_sha256Hash $(_s3_caching_sha256Hash $(_s3_caching_sha256Hash $(_s3_caching_sha256Hash $(_s3_caching_sha256Hash \
+      ${_s3_caching_key} ${_s3_caching_dateStamp}) ${_s3_caching_region}) ${_s3_caching_service}) "aws4_request") ${_s3_caching_stringToSign})
+    echo "AWS4-HMAC-SHA256 Credential=${_s3_caching_credential},SignedHeaders=${_s3_caching_signedHeaders},Signature=${_s3_caching_signature}"
 }
 
-function sha256Hash {
-    echo -en $2 | openssl dgst -sha256 -mac Hmac -macopt hexkey:$1 | trimOpenSslOutput
+function _s3_caching_sha256Hash {
+    echo -en $2 | openssl dgst -sha256 -mac Hmac -macopt hexkey:$1 | _s3_caching_trimOpenSslOutput
 }
 
-function trimOpenSslOutput {
+function _s3_caching_trimOpenSslOutput {
     cut -d ' ' -f 2 $1
 }
